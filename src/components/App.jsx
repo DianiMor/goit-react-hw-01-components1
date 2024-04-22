60d4571e04e81b850b99ce3f34f932a23f406399
DianiMor <diana.3mol@gmail.com>
2024-04-21 21:55:46 -0500

    agregando componentes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
+import React from 'react';
+
+import Profile from '../components/Profile/Profile';
+import user from '../json/user.json';
+
+import Statistics from '../components/Statistics/Statistics';
+import data from '../json/data.json';
+
+import FriendList from '../components/FriendList/FriendList';
+import friends from '../json/friends.json';
+
+import Transactions from '../components/Transactios/TransactionHistory';
+import transactions from '../json/transactions.json';
+
+const App = () => {
   return (
-    <div
-      style={{
+    <>
+      <h1>React Homework</h1>
+      {/*       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
-    >
-      React homework template
-    </div>
+    > */}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <h2>Statistics</h2>
+      <Statistics title="Upload stats" stats={data}></Statistics>
+      <h2>Friend List</h2>
+      <FriendList friends={friends}></FriendList>
+      <h2>Transactions</h2>
+      <Transactions items={transactions}></Transactions>
+    </>
   );
 };
+
+export default App;
